1fcfcd0146ee0f80fa1ed05d50b48ac5d2fb9219
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2022-03-23 15:26:58 -0400

    set up app to fetch our users from an API; set up the input and the functionality to be able to search thru our list of users

--- src/App.js
+++ src/App.js
@@ -1,40 +1,55 @@
 import { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-
+  // 1: constructor runs first always in any class
   constructor() {
     super();
-
+    // 2: the constructor is used to initialize the state
     this.state = {
-      monsters: [
-        {
-          id: '1',
-          name: 'Cody'
-        },
-        {
-          id: '2',
-          name: 'Maverick'
-        },
-        {
-          id: '3',
-          name: 'Bubbles'
-        },
-        {
-          id: '4',
-          name: 'Jack'
-        }
-      ]
-
-
+      monsters: [],
+      searchField: '',
     }
   }
+  // 4: componentDidMount runs after the render function
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(monsters => this.setState({ monsters }))
+      .catch(err => console.log(err));
+  }
 
+  onSearchChange = (event) => {
+    // making a search variable to lowercase the search string to help with case sensitivity
+    const searchField = event.target.value.toLocaleLowerCase();
+    // then we are going to set the state to the filtered monsters
+    this.setState(() => {
+      return { searchField }
+    }
+    )
+  }
+  // 3: render runs after the constructor sets the state for the first time
+  // 5: after componentDidMount runs, the render function is called again to reflect the changes that were made in the state
   render() {
+    const { monsters, searchField } = this.state
+    const { onSearchChange } = this;
+    // making a new variable to filter thru the monsters array 
+    const filteredMonsters = monsters.filter((monster) => {
+      // then we are going to check if the monster name includes the value of the search box
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    })
+
     return (
-      <div className="App">
-        {this.state.monsters.map((monster) => {
+      <div className='App'>
+        {/* SEARCH INPUT FIELD */}
+        <input
+          className='search-box'
+          type='search'
+          placeholder='Search Monsters'
+          onChange={onSearchChange}
+        />
+        {/*  RENDERS THE MONSTER LIST */}
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
